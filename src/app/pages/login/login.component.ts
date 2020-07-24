9d68509cdf096c52dccde857617ec013095321b5
Piyush696 <piyush.dutta@softobotics.com>
2020-07-25 01:03:35 +0530

    login & registration form builder

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,22 +7,19 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
-
-  constructor() { }
+  LoginForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-
+    this.LoginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    })
   }
 
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+  onLogin() {
+    console.log(this.LoginForm.value)
   }
+
 
 }
