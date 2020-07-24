6079cc1e7ff205068f82e4a85c1bd6ab08123c71
Piyush696 <piyush.dutta@softobotics.com>
2020-07-24 20:46:44 +0530

    changes done

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -7,7 +7,9 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  name = new FormControl('', [Validators.required, Validators.email]);
+  firstName = new FormControl('', [Validators.required]);
+  lastName = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl('', [Validators.required]);
   confirmPassword = new FormControl('', [Validators.required]);
   phone = new FormControl('', [Validators.required]);
@@ -17,10 +19,10 @@ export class RegistrationComponent implements OnInit {
   }
 
   getErrorMessage() {
-    if (this.name.hasError('required')) {
+    if (this.firstName.hasError('required')) {
       return 'You must enter a value';
     }
-    return this.name.hasError('email') ? 'Not a valid email' : '';
+    return this.firstName.hasError('email') ? 'Not a valid email' : '';
   }
 
 }
