0d3294709b522acac52b325a50e7cf6e0d55cb2b
Piyush696 <piyush.dutta@softobotics.com>
2020-07-30 17:45:52 +0530

    console removed

--- src/app/store/reducers/userInfo.reducer.ts
+++ src/app/store/reducers/userInfo.reducer.ts
@@ -7,7 +7,6 @@ export function UserInfoReducer(state: any = initialState, action: UserInfoActio
     switch (action.type) {
         case UserInfoTypes.ADD_USERINFO:
             return action.payload;
-
         default:
             return state;
     }
