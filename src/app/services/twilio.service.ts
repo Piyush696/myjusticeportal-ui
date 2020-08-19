2e112ebb67fe356e402ade90f7d8db1fc2fb6a1c
Piyush696 <piyush.dutta@softobotics.com>
2020-08-19 23:26:32 +0530

    twilio credentials update in app-setting for superadmin

---
+++ src/app/services/twilio.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { CacheService } from './cache.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TwilioService {
+  private apiPath: string;
+  constructor(private httpClient: HttpClient, private cacheService: CacheService) {
+    const env: any = environment;
+    this.apiPath = env.api
+  }
+
+  getHeaders() {
+    return {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${this.cacheService.getCache('token')}`
+      })
+    };
+  }
+
+  getTwilioCredentials() {
+    return this.httpClient.get<object>(`${this.apiPath}/twilio`, this.getHeaders())
+  }
+
+  updateTwilioCredentials(updateTwilioCredencialData) {
+    return this.httpClient.post<object>(`${this.apiPath}/twilio/twilio`, updateTwilioCredencialData, this.getHeaders())
+  }
+}
