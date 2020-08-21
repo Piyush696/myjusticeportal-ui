55f58d0e00d0d638e51e003d4958fdd311003b4c
akshay-4362 <akshay.t@softobotics.com>
2020-08-21 14:15:33 +0530

    field name changes and case model name changes

--- src/app/pages/case-form/case-form.component.ts
+++ src/app/pages/case-form/case-form.component.ts
@@ -27,10 +27,10 @@ export class CaseFormComponent implements OnInit, OnChanges {
     this.caseForm = this.fb.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
-      countyOfArrest: ['', [Validators.required]],
-      dateOfArrest: ['', [Validators.required]],
-      caseRelatedTo: ['', [Validators.required]],
-      caseJurisdiction: ['', [Validators.required]],
+      countyOfArrest: [''],
+      dateOfArrest: [''],
+      briefDescriptionOfChargeOrLegalMatter: ['', [Validators.required]],
+      attorneyName: [''],
       nextCourtDate: ['', [Validators.required]],
       legalRepresentation: ['', [Validators.required]],
     });
@@ -46,8 +46,8 @@ export class CaseFormComponent implements OnInit, OnChanges {
       this.caseForm.get('lastName').setValue(this.caseDetails.user.lastName)
       this.caseForm.get('countyOfArrest').setValue(this.caseDetails.countyOfArrest)
       this.caseForm.get('dateOfArrest').setValue(this.caseDetails.dateOfArrest)
-      this.caseForm.get('caseRelatedTo').setValue(this.caseDetails.caseRelatedTo)
-      this.caseForm.get('caseJurisdiction').setValue(this.caseDetails.caseJurisdiction)
+      this.caseForm.get('briefDescriptionOfChargeOrLegalMatter').setValue(this.caseDetails.briefDescriptionOfChargeOrLegalMatter)
+      this.caseForm.get('attorneyName').setValue(this.caseDetails.attorneyName)
       this.caseForm.get('nextCourtDate').setValue(this.caseDetails.nextCourtDate)
       this.caseForm.get('legalRepresentation').setValue(this.caseDetails.legalRepresentation)
     }
