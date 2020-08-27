9de2c3982662ea4d415a21c7903b20bd96541cde
akshay-4362 <akshay.t@softobotics.com>
2020-08-27 16:40:19 +0530

    country code change

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -25,9 +25,7 @@ export class LoginComponent implements OnInit {
     this.loginForm = this.fb.group({
       userName: ['', [Validators.required]],
       password: ['', [Validators.required]],
-      otp: ['', [Validators.required]],
-      countryCode: ['01', [Validators.required]],
-      mobile: ['', [Validators.required]],
+      otp: ['', [Validators.required]]
     })
   }
 
