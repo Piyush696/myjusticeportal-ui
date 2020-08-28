73d56f9c8a5adacd7522e34ed5ba1c7ec3180050
Piyush696 <piyush.dutta@softobotics.com>
2020-08-28 16:11:48 +0530

    case notes

--- src/app/pages/cases/cases.component.ts
+++ src/app/pages/cases/cases.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { CaseService } from 'app/services/case.service';
+import { ToasterService } from 'app/services/toaster.service';
 
 @Component({
   selector: 'app-cases',
@@ -11,11 +14,20 @@ import { CaseService } from 'app/services/case.service';
 export class CasesComponent implements OnInit {
   caseList: any;
   caseData: any;
-
-  constructor(private caseService: CaseService, private route: Router) { }
+  caseNoteForm: FormGroup;
+  buttonStatus: string = 'Edit';
+  caseId: any;
+  constructor(private router: Router, private toasterService: ToasterService, public dialog: MatDialog, private caseService: CaseService, private route: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.getCases();
+    this.createCaseNotesForm()
+  }
+
+  createCaseNotesForm() {
+    this.caseNoteForm = this.fb.group({
+      notes: ['', [Validators.required]]
+    })
   }
 
   getCases() {
@@ -24,7 +36,29 @@ export class CasesComponent implements OnInit {
     })
   }
 
+  openModal(templateRef, value) {
+    console.log(templateRef, value)
+    this.caseId = value.caseId
+    let dialogRef = this.dialog.open(templateRef, {
+      width: '500px',
+    });
+    this.caseNoteForm.get('notes').setValue(value.notes)
+    this.caseNoteForm.get('notes').disable();
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
+
   onViewCase(caseId) {
     this.route.navigateByUrl('/case/' + caseId);
+  }
+
+  onSaveChanges() {
+    this.caseService.updateCase(this.caseNoteForm.value, this.caseId).subscribe((res: any) => {
+      this.caseNoteForm.disable();
+      this.dialog.closeAll();
+      this.getCases();
+      this.buttonStatus = 'Edit';
+      this.toasterService.showSuccessToater('Notes Updated Successfully.')
+    })
   }
 }
