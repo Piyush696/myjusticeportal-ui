668221b3459bb57671339134fa7ae29f452fc0d4
Piyush696 <piyush.dutta@softobotics.com>
2020-09-03 21:45:47 +0530

    facility registration with facility code & mabile authenticate ui and verfy sms ui

---
+++ src/app/services/registration/facility.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CacheService } from '../cache.service';
+import { environment } from 'environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FacilityService {
+  private apiPath: string;
+  allUsers: string;
+
+  constructor(private httpClient: HttpClient, private cacheService: CacheService) {
+    const env: any = environment;
+    this.apiPath = env.api;
+  }
+
+  onRegistration(registrationData) {
+    return this.httpClient.post<object>(`${this.apiPath}/facility-registration/registration`, registrationData);
+  }
+
+  authenticateMobile(mobileData) {
+    return this.httpClient.post<object>(`${this.apiPath}/facility-registration/authenticate/registration`, mobileData);
+  }
+
+  verifySms(verifyData) {
+    return this.httpClient.post<object>(`${this.apiPath}/facility-registration/verify-sms/registration`, verifyData);
+  }
+}
