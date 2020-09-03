77187be3031068b841d72bc38788b27795789016
Rajeshmaharana990 <59282611+Rajeshmaharana990@users.noreply.github.com>
2020-09-04 00:28:26 +0530

    button-purple added in styles.scss

--- src/app/pages/app-settings/app-settings.component.ts
+++ src/app/pages/app-settings/app-settings.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-app-settings',
   templateUrl: './app-settings.component.html',
-  styleUrls: ['./app-settings.component.css']
+  styleUrls: ['./app-settings.component.scss']
 })
+
 export class AppSettingsComponent implements OnInit {
 
   constructor() { }
