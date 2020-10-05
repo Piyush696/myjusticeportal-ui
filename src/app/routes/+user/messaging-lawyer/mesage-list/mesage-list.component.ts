2819d24d8b3baaa147daefe81b67b4a6b0f63db8
akshay-4362 <akshay.t@softobotics.com>
2020-10-05 17:20:31 +0530

    socket chat

--- src/app/routes/+user/messaging-lawyer/mesage-list/mesage-list.component.ts
+++ src/app/routes/+user/messaging-lawyer/mesage-list/mesage-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -6,16 +6,45 @@ import { MatDialog } from '@angular/material/dialog';
   templateUrl: './mesage-list.component.html',
   styleUrls: ['./mesage-list.component.css']
 })
-export class MesageListComponent implements OnInit {
+export class MesageListComponent implements OnInit, OnChanges {
   @Input() userMessageList: any
+  @Input() oldUserList: any
+  @Input() userList: any;
   @Output() messageEvent = new EventEmitter()
+  @Output() SubmitEvent = new EventEmitter()
   selectedUser: any;
+  selectedLawyer: any;
+  show: any;
 
   constructor(public dialog: MatDialog,) { }
-
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    console.log(this.userList)
+    if (this.userList) {
+      this.oldUserList = this.userList
+      this.show = false
+    } else if (this.userMessageList) {
+      this.show = true
+    }
   }
 
+  ngOnInit(): void {
+    console.log(this.userMessageList)
+  }
+
+  onNativeChange(event, user) {
+    this.selectedLawyer = user
+    console.log(event, user)
+    if (event) {
+      this.oldUserList.push(user);
+    } else {
+      this.oldUserList.forEach((x, i, a) => {
+        if (x.userId == user.userId) {
+          this.oldUserList.splice(i, 1)
+        }
+
+      })
+    }
+  }
 
   onClicklawyer(lawyerId) {
     this.selectedUser = lawyerId;
@@ -30,5 +59,19 @@ export class MesageListComponent implements OnInit {
     let dialogRef = this.dialog.open(templateRef, {
       width: '500px',
     });
+  }
+
+  onEmailInvite() {
+    let lawyerArray = [this.selectedLawyer]
+    console.log(this.selectedLawyer)
+    let dialogRef = this.dialog.closeAll()
+    this.userMessageList = this.userMessageList.filter(item1 =>
+      !lawyerArray.some(item2 => (item2.userId === item1.userId)))
+
+  }
+
+  closeModal() {
+    let dialogRef = this.dialog.closeAll()
+
   }
 }
