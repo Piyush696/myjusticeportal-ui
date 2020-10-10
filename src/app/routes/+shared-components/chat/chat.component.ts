b080744989668977a19f51d721f61f7c935a2e71
akshay-4362 <akshay.t@softobotics.com>
2020-10-10 17:57:11 +0530

    restricted both user and lawyer from sending the empty message

--- src/app/routes/+shared-components/chat/chat.component.ts
+++ src/app/routes/+shared-components/chat/chat.component.ts
@@ -19,7 +19,7 @@ export class ChatComponent implements OnInit, OnChanges {
   @Input() allMessages = [];
 
   socket;
-  message: string;
+  message: string = "";
   userId;
   userInfo: any;
   messageList: any;
@@ -56,15 +56,18 @@ export class ChatComponent implements OnInit, OnChanges {
   }
 
   SendMessage() {
-    const data = {
-      "receiverId": this.receiverId,
-      "senderId": this.userId,
-      "message": this.message,
-      "createdAt": new Date()
+    console.log(this.message)
+    if (this.message !== '') {
+      const data = {
+        "receiverId": this.receiverId,
+        "senderId": this.userId,
+        "message": this.message,
+        "createdAt": new Date()
+      }
+      this.allMessages.push(data)
+      this.socket.emit('message', data);
+      this.message = ''
     }
-    this.allMessages.push(data)
-    this.socket.emit('message', data);
-    this.message = ''
   }
 
 }
