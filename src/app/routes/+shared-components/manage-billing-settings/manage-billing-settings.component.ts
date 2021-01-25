27ece77a0355b0fc332e35974381bacbafcad65a
akshay-4362 <akshay.t@softobotics.com>
2021-01-25 14:20:19 +0530

    defender update plan logic

--- src/app/routes/+shared-components/manage-billing-settings/manage-billing-settings.component.ts
+++ src/app/routes/+shared-components/manage-billing-settings/manage-billing-settings.component.ts
@@ -11,22 +11,85 @@ import { UserMetaService } from 'app/services/user-meta.service';
   styleUrls: ['./manage-billing-settings.component.css']
 })
 export class ManageBillingSettingsComponent implements OnInit {
-  facilities = [];
-  planPrice: number = 0;
-  currentPlanPrice: number = 40;
-  totalBillingPrice: number = 40;
+  selectedFacilities = [];
+  planPrice: number = 40;
+  totalPrice: number = 40;
+  state = [];
+  facilityList = [];
+  facilityId: any;
+  spinner: any;
+  isDisabled: boolean = true;
 
-  constructor(private defenderService: DefenderService) { }
+  constructor(private defenderService: DefenderService, private userMetaService: UserMetaService, private facilityService: FacilityService,) { }
 
   ngOnInit(): void {
     this.getBillableFacility();
+    this.getUserDetails();
   }
-
 
   getBillableFacility() {
     this.defenderService.getBilliableFacilityDetails().subscribe((data: any) => {
-      this.facilities = data.facilities
+      this.selectedFacilities = data.facilities
     })
   }
+
+  getUserDetails() {
+    this.userMetaService.getUserAdditionalDetails().subscribe((user: any) => {
+      user.data.forEach((ele) => {
+        if (ele.metaKey == "State:Bar") {
+          let splitArray = ele.metaValue.split(":")
+          this.state.push(splitArray[0].toString());
+        }
+      })
+      this.getAllFacilities();
+    })
+  }
+
+  getAllFacilities() {
+    this.facilityService.getFacilitiesUserCount().subscribe((facilities: any) => {
+      if (facilities.data) {
+        this.facilityList = facilities.data.map((ele) => {
+          if (this.state.includes(ele.Address.state)) {
+            const isFacilitly = this.selectedFacilities.find(x => x.facilityId === ele.facilityId)
+            if (isFacilitly) {
+              ele['isSelected'] = isFacilitly.isSelected;
+            } else {
+              ele['isSelected'] = false;
+            }
+            return ele;
+          }
+        })
+        this.facilityList = this.facilityList.filter(x => x)
+      }
+    })
+  }
+
+  onFacilitySelect(event, facilityId) {
+    if (event) {
+      this.facilityId = facilityId
+      this.facilityList = this.facilityList.map((facility) => {
+        if (facility.facilityId === facilityId) {
+          facility.isSelected = true;
+        }
+        return facility
+      })
+    } else {
+      this.facilityList.map((facility) => {
+        if (facility.facilityId === facilityId) {
+          facility.isSelected = false;
+        }
+        return facility
+      })
+    }
+  }
+
+  onEdit() {
+    this.isDisabled = false
+  }
+
+  startLoader(value) {
+    this.spinner = value
+  }
+
 
 }
