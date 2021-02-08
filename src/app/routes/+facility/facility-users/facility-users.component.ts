3d96cb2d4ca4b450844c9bd80dfc940468451b0b
Piyushsofto <piyusd.dutta@softobotics.com>
2021-02-08 16:56:49 +0530

    facility users facility admin & assign facility done

--- src/app/routes/+facility/facility-users/facility-users.component.ts
+++ src/app/routes/+facility/facility-users/facility-users.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { FacilityService } from 'app/services/facility.service';
 
 @Component({
   selector: 'app-facility-users',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacilityUsersComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ["name", "username", "email", "mobile"];
+  dataSource = new MatTableDataSource();
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  
+  constructor(private facilityService:FacilityService) { }
 
   ngOnInit(): void {
+    this.getAllFacilityUsers();
+  }
+
+  search(searchValue: string) {
+    let s=searchValue.replace(/  +/g, ' ');
+    s= searchValue.split(" ").join("")
+    this.dataSource.filter = s.trim().toLowerCase();
+ }
+ 
+  getAllFacilityUsers(){
+    this.facilityService.getFacilityUsers().subscribe((users:any)=>{
+      console.log(users)
+      this.dataSource.sortingDataAccessor = (item: any, property) => {
+        switch (property) {
+          case 'name': if (item) return item.firstName + item.middleName + item.lastName;
+            return item[property]
+        }
+      };
+      this.dataSource = new MatTableDataSource(users.data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+  }
+
+
+  getPageSizeOptions(): number[] {
+    if (this.dataSource.data.length > 500)
+      return [10, 50, 100, 500, this.dataSource.paginator?.length];
+    else if (this.dataSource.data.length > 100) {
+      return [10, 50, 100, this.dataSource.paginator?.length];
+    }
+    else if (this.dataSource.data.length > 50) {
+      return [10, 50, this.dataSource.paginator?.length];
+    }
+    else if (this.dataSource.data.length > 10) {
+      return [10, this.dataSource.paginator?.length];
+    }
+    else {
+      return [10];
+    }
   }
 
 }
